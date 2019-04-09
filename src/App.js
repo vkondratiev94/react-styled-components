cdc95b424cb27babf634a9b9e0bb4269727b90c5
Vlad Kondratiev <vlad.kondratiev@ekreative.com>
2019-04-09 10:09:53 +0300

    styling existing components & extending

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ const Heading = styled.h1`
 
 const color = 'white'
 
+// Passing props and using variables
 const Button = styled.button`
   background-color: ${({ type }) => type === 'cancel' ? 'tomato' : 'indigo'};
   border-radius: 4px;
@@ -18,14 +19,35 @@ const Button = styled.button`
   color: ${color};
 `
 
+// Extending styles
+const OutlinedButton = styled(Button)`
+  background-color: transparent;
+  border: white;
+`
+
+// Nesting, referencing styled component
 const AppWrapper = styled.div`
   header {
     background-color: teal;
 
-    &:hover {
-      background-color: red;
-    }
+    // &:hover {
+    //   background-color: red;
+    // }
   }
+  ${Button} {
+    margin-bottom: 1rem;
+  }
+`
+
+// styling existing component
+const Fake = ({ className }) => (
+  <div className={className}>
+    <h2>I'm a fake component</h2>
+  </div>
+)
+
+const StyledFake = styled(Fake)`
+  color: red;
 `
 
 class App extends Component {
@@ -34,8 +56,10 @@ class App extends Component {
       <AppWrapper>
         <header className="App-header">
           <Heading>Hello styled-components!</Heading>
+          <StyledFake />
           <Button>Save</Button>
           <Button type='cancel'>Cancel</Button>
+          <OutlinedButton>Outlined</OutlinedButton>
           <img src={logo} className="App-logo" alt="logo" />
           <p>
             Edit <code>src/App.js</code> and save to reload.
